d93eec2cc4b8a37aa59a21e165e8878843ea8dcb
dev_yang <13143231178@163.com>
2017-05-14 02:39:41 +0000

    add a;

---
+++ src/test.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!dd!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!w !" << endl; // prints !!!Hello World!!!
+	int a=0;
+	a+=1;
+	cout<<a<<endl;
+	return 0;
+}
